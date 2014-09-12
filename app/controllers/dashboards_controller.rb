60fddb0351e45322fcb33d80f54e6e3250fc7a70
Seth Geyer <seth.geyer@gmail.com>
2014-09-12 14:20:11 -0600

    added ability to see other rants

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,7 +1,8 @@
 class DashboardsController < ApplicationController
   def show
     @rant = Rant.new
-    @rants = kenny_loggins.rants
+    @my_rants = kenny_loggins.rants
+    @others_rants = Rant.where('user_id != ?', kenny_loggins.id)
 
   end
 end
--- spec/features/dashboard_spec.rb
+++ spec/features/dashboard_spec.rb
@@ -1,6 +1,9 @@
 feature "Dashboard Page" do
   before(:each) do
-    create_user
+    create_rant(1, {user_id: 99})
+    create_rant(2, {user_id: 99})
+    @user = create_user
+    create_rant(3, {user_id: @user.id})
     visit_login_page_and_fill_in_form('seth', 'password')
     click_on "Login"
   end
@@ -14,6 +17,21 @@ feature "Dashboard Page" do
   scenario "As a user, I can post my rants" do
     expect(page).to have_css(".rant-form")
   end
+
+
+  scenario "As a user, I can view my rants" do
+    expect(page).to have_css(".my-rants")
+    expect(page.find(".my-rants")).to have_content("I've got rants in my pants #3")
+  end
+
+  scenario "As a user, I can view others rants" do
+    expect(page.find(".others-rants")).to have_content("I've got rants in my pants #1")
+    expect(page.find(".others-rants")).to have_content("I've got rants in my pants #2")
+    expect(page.find(".others-rants")).not_to have_content("I've got rants in my pants #3")
+
+  end
+
+
 
 
 
