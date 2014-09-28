a084c50caefb4a8ea4d5e82300767c5faebe216f
Seth Geyer <seth.geyer@gmail.com>
2014-09-28 11:25:50 -0600

    corrected error in testing incorrect login

--- spec/features/session_spec.rb
+++ spec/features/session_spec.rb
@@ -1,18 +1,16 @@
 
 feature "user login and logout" do
 
-  before(:each) do
+  scenario "As a registered user, I can login" do
     create_user
     visit_login_page_and_fill_in_form("seth", "password")
-  end
-
-
-  scenario "As a registered user, I can login" do
     within("#new-sessions") {click_on "Login"}
     expect(page).to have_content("Latest Rants")
   end
 
   scenario "As a registered user, I can't login with incorrect credentials" do
+    create_user
+    visit_login_page_and_fill_in_form("", "password")
     within("#new-sessions") {click_on "Login"}
     expect(page).to have_css("#new-sessions")
     expect(page).to have_content("Login failed")
@@ -20,13 +18,21 @@ feature "user login and logout" do
     within("#new-sessions") {click_on "Login"}
     expect(page).to have_css("#new-sessions")
     expect(page).to have_content("Login failed")
+
   end
 
   scenario "As a logged in user, I can logout" do
+    create_user
+    visit_login_page_and_fill_in_form("seth", "password")
     within("#new-sessions") {click_on "Login"}
     expect(page).to have_content("Latest Rants")
     click_on "Logout"
     expect(page).to have_link("Login")
   end
+
+
+
+
+
 
 end
