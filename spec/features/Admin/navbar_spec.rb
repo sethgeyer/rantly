0aad18b6345a13e07aff3b50fdbeabea850f71b9
Seth Geyer <seth.geyer@gmail.com>
2014-10-12 11:54:58 -0600

    an admin user sees their own unique navbar

--- spec/features/Admin/navbar_spec.rb
+++ spec/features/Admin/navbar_spec.rb
@@ -1,11 +1,18 @@
 feature "Admin Navbar" do
 
   before(:each) do
-    create_user({username: "admin", password: "password", first_name: "Ad", last_name: "Min", admin: true})
+    create_user({username: "admin", password: "password", first_name: "Ad", last_name: "Min", is_admin: true})
+    visit_login_page_and_fill_in_form("admin", "password")
+    within("#new-sessions") {click_on "Login"}
   end
 
 
   scenario "As an admin, my navbar should include admin-specific navigation tools" do
+    expect(page).to have_content("Dashboard")
+    expect(page).to have_content("Rants")
+    expect(page).to have_content("Users")
+    expect(page).not_to have_content("Favorites")
+    expect(page).not_to have_content("Following")
 
   end
 
