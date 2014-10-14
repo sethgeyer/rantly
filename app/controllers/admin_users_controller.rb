a5d91984101459435b0805be2eef1218d8a1b9de
Seth Geyer <seth.geyer@gmail.com>
2014-10-14 10:10:47 -0600

    admin can sort users by number of rants

--- app/controllers/admin_users_controller.rb
+++ app/controllers/admin_users_controller.rb
@@ -3,7 +3,17 @@ class AdminUsersController < ApplicationController
   before_action :ensure_current_user_is_admin
 
   def index
-    @users = User.all
+    if params[:sort_order] == "desc"
+      @users = User.all.sort_by { |user| -user.rants.count}
+      @sorter = "asc"
+    elsif params[:sort_order] == "asc"
+      @users = User.all.sort_by { |user| user.rants.count}
+      @sorter = "desc"
+
+    else
+      @users = User.all
+    end
+
   end
 
 
--- spec/features/Admin/admin_rights_spec.rb
+++ spec/features/Admin/admin_rights_spec.rb
@@ -5,6 +5,12 @@ feature "Admin Rights" do
     @first_rant = create_rant(3, {topic: "first_rant_created", user_id: @user.id})
     @second_rant = create_rant(1, {topic: "second_rant_created", user_id: @user.id})
     @third_rant = create_rant(2, {topic: "third_rant_created", user_id: @user.id})
+    @second_user = create_user({first_name: "Steve", last_name: "Smith", username: "steve"})
+    @fourth_rant = create_rant(4, {topic: "fourth_rant_created", user_id: @second_user.id})
+    @third_user = create_user({first_name: "Alex", last_name: "Firoved", username: "alex"})
+    @fifth_rant = create_rant(5, {topic: "fifth_rant_created", user_id: @third_user.id})
+    @sixth_rant = create_rant(6, {topic: "fifth_rant_created", user_id: @third_user.id})
+
   end
 
 
@@ -30,20 +36,20 @@ feature "Admin Rights" do
       within("#new-sessions") {click_on "Login"}
     end
 
-    scenario "As an admin, I can view a list of all rants in the system ordered by date" do
+    scenario "I can view a list of all rants in the system ordered by date" do
       click_on "Rants"
       expect(page).to have_content("third_rant_created")
       expect(page).to have_content("Seth Geyer")
     end
 
-    scenario "As an admin, I can view a list of all users and their total rants" do
+    scenario "I can view a list of all users and their total rants" do
       click_on "Users"
       expect(page).to have_content("Seth Geyer")
       expect(page).to have_content("3")
       expect(page).to have_content("Ad Min")
     end
 
-    scenario "Aa an admin, I can disable a user from being able to login" do
+    scenario "I can disable a user from being able to login" do
       click_on "Users"
       first(".disable-link").click
       expect(User.find(@user.id).is_disabled).to eq(true)
@@ -51,7 +57,26 @@ feature "Admin Rights" do
       visit_login_page_and_fill_in_form("seth", "password")
       within("#new-sessions") {click_on "Login"}
       expect(page).to have_content("Your account has been disabled")
+    end
 
+    scenario "I can sort users by most rants submitted" do
+      click_on "Users"
+      find(".sorter").click
+      within("tr.ranters-row:nth-child(1)") { expect(page).to have_content("Seth")}
+      within("tr.ranters-row:nth-child(2)") { expect(page).to have_content("Alex")}
+      within("tr.ranters-row:nth-child(3)") { expect(page).to have_content("Steve")}
+      within("tr.ranters-row:nth-child(4)") { expect(page).to have_content("Ad")}
+
+    end
+
+    scenario "I can sort users by least rants submitted" do
+      click_on "Users"
+      find(".sorter").click
+      find(".sorter").click
+      within("tr.ranters-row:nth-child(1)") { expect(page).to have_content("Ad")}
+      within("tr.ranters-row:nth-child(2)") { expect(page).to have_content("Steve")}
+      within("tr.ranters-row:nth-child(3)") { expect(page).to have_content("Alex")}
+      within("tr.ranters-row:nth-child(4)") { expect(page).to have_content("Seth")}
     end
 
 
