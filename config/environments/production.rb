d1a1fad0da1c2c831acdde8748056bbd8d997809
Seth Geyer <seth.geyer@gmail.com>
2014-10-14 16:55:05 -0600

    added sendgrid variables and configuration for action mailer.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -44,6 +44,22 @@ Rails.application.configure do
   # Set to :debug to see everything in the log.
   config.log_level = :info
 
+
+
+  ActionMailer::Base.smtp_settings = {
+    :user_name => ENV['SENDGRID_USERNAME'],
+    :password => ENV['SENDGRID_PASSWORD'],
+    :domain => 'http://spewing-thoughts.herokuapp.com/',
+    :address => 'smtp.sendgrid.net',
+    :port => 587,
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
+
+  config.action_mailer.default_url_options = { :host => 'spewing-thoughts.heroku.com' }
+
+
+
   # Prepend all log lines with the following tags.
   # config.log_tags = [ :subdomain, :uuid ]
 
