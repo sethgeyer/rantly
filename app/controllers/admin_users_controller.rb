d69564e162e0ee847e054bad1f1a71aa17910b4e
Seth Geyer <seth.geyer@gmail.com>
2014-10-17 11:36:50 -0600

    refactored users controller

--- app/controllers/admin_users_controller.rb
+++ app/controllers/admin_users_controller.rb
@@ -3,19 +3,18 @@ class AdminUsersController < ApplicationController
   before_action :ensure_current_user_is_admin
 
   def index
+
+
     if params[:sort_order] == "desc"
-      @users = User.all.sort_by { |user| -user.rants.count}
+      @users = User.all.sort_by { |user| user.rants.count}.reverse
       @sorter = "asc"
     elsif params[:sort_order] == "asc"
       @users = User.all.sort_by { |user| user.rants.count}
       @sorter = "desc"
-
     else
       @users = User.all
     end
-
   end
-
 
   def update
     @user = User.find(params[:id])
