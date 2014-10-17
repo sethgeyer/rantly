502d84a599eac07f59d86acb62cc11045c630371
Seth Geyer <seth.geyer@gmail.com>
2014-10-17 10:14:24 -0600

    refactored interesting ranters controller

--- app/controllers/interesting_ranters_controller.rb
+++ app/controllers/interesting_ranters_controller.rb
@@ -1,19 +1,18 @@
 class InterestingRantersController < ApplicationController
 
-  def create
-    interesting_ranter = InterestingRanter.new
-    interesting_ranter.user_id = kenny_loggins.id
-    interesting_ranter.person_id = params[:interesting_ranter_id]
-    interesting_ranter.save!
-    render :status => :created, :json => interesting_ranter
-  end
-
   def index
     @interesting_ranters = kenny_loggins.interesting_ranters
   end
 
+  def create
+    interesting_ranter = kenny_loggins.interesting_ranters.create!(
+      person_id: params[:interesting_ranter_id]
+    )
+    render :status => :created, :json => interesting_ranter
+  end
+
   def destroy
-    InterestingRanter.find(params[:id]).destroy
+    kenny_loggins.interesting_ranters.find(params[:id]).destroy
     render :nothing => true
   end
 
