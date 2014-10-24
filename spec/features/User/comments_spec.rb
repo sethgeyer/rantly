5307db8f5a6dd05da6aa27bce83ebce1f5ff4131
Seth Geyer <seth.geyer@gmail.com>
2014-10-24 11:03:20 -0600

    moved comment form to a better spot in the show view of a rant"

--- spec/features/User/comments_spec.rb
+++ spec/features/User/comments_spec.rb
@@ -6,16 +6,35 @@ feature "create and view comments" do
     create_rant(2, {user_id: @seth.id})
     visit_login_page_and_fill_in_form('seth', 'password')
     within("#new-sessions") {click_on "Login"}
-    visit rant_path(@rant_to_visit.id)
   end
 
   scenario "As a user visiting a rant show page, I should see a comment form" do
+    visit rant_path(@rant_to_visit.id)
     expect(page).to have_css("#new_comment")
     fill_in "comment[body]", with: "This post rules"
     click_on "Rant Back"
     expect(page).to have_button("Rant Back")
     expect(page).to have_content("This post rules")
-
   end
+
+  scenario "As a user visiting a rant show page, I should see a comment form" do
+    visit rant_path(@rant_to_visit.id)
+    expect(page).to have_css(".new_comment")
+    fill_in "comment[body]", with: "This post rules"
+    click_on "Rant Back"
+    expect(page).to have_button("Rant Back")
+    expect(page).to have_content("This post rules")
+  end
+
+  scenario "As a user visiting a users profile page, I should be able to comment" do
+    visit profile_path(@adam)
+    expect(page).to have_css(".new_comment")
+    fill_in "comment[body]", with: "This profile rules"
+    click_on "Rant Back"
+    expect(page).to have_button("Rant Back")
+    expect(page).to have_content("This profile rules")
+  end
+
+
 
 end
