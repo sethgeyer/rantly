af875f2afc18b545c6a26b9537ac772c9ac3bbf0
Seth Geyer <seth.geyer@gmail.com>
2014-11-11 15:49:33 -0700

    added the keen io graph

--- app/controllers/admin_dashboards_controller.rb
+++ app/controllers/admin_dashboards_controller.rb
@@ -1,20 +1,11 @@
 class AdminDashboardsController < ApplicationController
 
-
   def show
-    @data = {
-      "result" => [
-        {
-          "value" => 57,
-          "timeframe" => {
-            "start" => "2014-10-16T06:00:00.000Z",
-            "end" => "2014-10-17T06:00:00.000Z"
-          }
-        }
-      ]
-    }
+    @keen_list = KeenMetric.find_logins_per_hour
 
+
+
+    # puts "fudge"
   end
-
 
 end
--- app/models/keen_metric.rb
+++ app/models/keen_metric.rb
@@ -17,9 +17,19 @@ class KeenMetric
 
 
   def self.find_logins_per_hour
-    response = connection.get("/3.0/projects/#{ENV["KEEN_PROJECT_ID"]}/queries/count?api_key=#{ENV["KEEN_READ_KEY"]}&event_collection=logins&timeframe=today&timezone=-21600&interval=daily")
-    formatted = JSON.parse(response.body)
-    count = formatted["result"][0]
+    response = connection.get("/3.0/projects/#{ENV["KEEN_PROJECT_ID"]}/queries/count?api_key=#{ENV["KEEN_READ_KEY"]}&event_collection=logins&timeframe=this_23_hours&timezone=-21600&group_by=login_date")
+
+    dates = JSON.parse(response.body)["result"]
+
+    logged_hours = dates.map { |record| record["login_date"][11..12] }
+    @time = {"1"=> 3, "2"=> 4, "3"=>5, "4"=>0,"5"=>0,"6"=>0,"7"=>0,"8"=>0,"9"=>0,"10"=>0,"11"=>0,"12"=>0,"13"=>0,"14"=>0,"15"=>0,"16"=>0,"17"=>0,"18"=>0,"19"=>0,"20"=>0,"21"=>0,"22"=>0,"23"=>0,"24"=>0}
+
+    logged_hours.each do |hour|
+      @time[hour] += 1
+    end
+
+    @time
+
   end
 
 
